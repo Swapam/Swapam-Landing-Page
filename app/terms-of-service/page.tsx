3b8ff25bc20d80d1a24ce14fafbdaf0300f4e525
mxnuchim <manuchimoliver779@gmail.com>
2024-12-26 00:09:18 +0100

    fix: privacy

--- app/terms-of-service/page.tsx
+++ app/terms-of-service/page.tsx
@@ -1,7 +1,19 @@
+"use client";
 import React from "react";
 
 const TermsOfService = () => {
-  return <div>TermsOfService</div>;
+  return (
+    <div
+      id="privacy-policy"
+      className="bg-white min-h-screen flex flex-col px-4 lg:px-12 justify-start items-start lg:items-center py-10 lg:py-20  transition duration-300 ease-in-out"
+    >
+      <div className="w-full">
+        <h2 className="text-left text-black py-16 text-2xl md:text-3xl lg:text-4xl font-semibold">
+          Terms of Service
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default TermsOfService;
